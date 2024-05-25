0b36315d8e59818fb53f04e8d8018621c9b143e4
Aleksey-Markov <tte4enuxa@gmail.com>
2024-05-25 21:22:29 +0700

    initial

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
-# Register your models here.
+from catalog.models import Product, Category
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('category_name', 'title', 'price',)
+    list_filter = ('category_name',)
+    search_fields = ('title', 'description',)
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('title',)
--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    title = models.CharField(max_length=100, verbose_name='наименование категории')
+    description = models.CharField(max_length=100, verbose_name='описание')
+
+    def __str__(self):
+        return f'{self.title}'
+
+    class Meta:
+        verbose_name = 'категория'
+        verbose_name_plural = 'категории'
+
+
+class Product(models.Model):
+    title = models.CharField(max_length=100, verbose_name='название продукта')
+    description = models.TextField(verbose_name='описание')
+    category_name = models.ForeignKey(Category, on_delete=models.SET_NULL, verbose_name='категория', null=True, blank=True, related_name='products')
+    image = models.ImageField(upload_to='product/photo',verbose_name='превью', null=True, blank=True)
+    price = models.IntegerField(verbose_name='цена')
+    created_at = models.DateField(verbose_name='дата создания')
+    updated_at = models.DateField(verbose_name='дата изменения')
+    manufactured_at = models.DateField(verbose_name='дата изготовления', null=True, blank=True)
+
+    def __str__(self):
+        return f'{self.category_name} {self.title}'
+
+    class Meta:
+        verbose_name = 'продукт'
+        verbose_name_plural = 'продукты'
